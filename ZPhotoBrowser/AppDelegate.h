d06c26bc3a04bf0d49e20e364a71e08d971b299a
cAibDe <you@example.com280206014@qq.com>
2017-06-22 08:57:22 +0800

    Initial Commit

---
+++ ZPhotoBrowser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZPhotoBrowser
+//
+//  Created by 张鹏 on 2017/6/22.
+//  Copyright © 2017年 c4ibD3. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZPhotoBrowserTests/ZPhotoBrowserTests.m
@@ -0,0 +1,39 @@
+//
+//  ZPhotoBrowserTests.m
+//  ZPhotoBrowserTests
+//
+//  Created by 张鹏 on 2017/6/22.
+//  Copyright © 2017年 c4ibD3. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ZPhotoBrowserTests : XCTestCase
+
+@end
+
+@implementation ZPhotoBrowserTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
